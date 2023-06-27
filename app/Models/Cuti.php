4e167c162d53213293f805a36b37b5b043a0f739
GINN30 <adyagi14@gmail.com>
2023-06-27 22:40:43 +0700

    buat models cuti dan karyawan

---
+++ app/Models/Cuti.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cuti extends Model
+{
+    use HasFactory;
+    public function karyawan()
+    {
+        return $this->belongsTo(Karyawan::class, 'nomer_induk', 'nomer_induk');
+    }
+}
